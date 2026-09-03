87c98945f68cd91a2058724a1c9cf27bbcdeba54
ybnr05 <ybnr05@gmail.com>
2026-09-03 15:48:26 -0400

    character report

--- Day01/Program.cs
+++ Day01/Program.cs
@@ -6,12 +6,33 @@ class Program
     {
         Console.Write("Enter your name:");
         string playerName = Console.ReadLine();
-        Console.Write("Enter your age: ");
-        int age = int.Parse(Console.ReadLine());
+        Console.Write("Enter  level: ");
+        int level = int.Parse(Console.ReadLine());
 
+        Console.Write("Enter Health:");
+        int health = int.Parse(Console.ReadLine());
+        Console.Write("Enter Gold");
+        int gold = int.Parse(Console.ReadLine());
+        Console.Write("Enter Speed:");
+        int speed = int.Parse(Console.ReadLine());
+
+        int levelAfter = level + 1;
+        int healthfter = health + 10;
+        int goldAfrer = gold + 50;
         Console.WriteLine();
-        Console.WriteLine("======player information======");
-        Console.WriteLine("Name:" + playerName);
-        Console.WriteLine("Age:" + age);
+        Console.WriteLine("===========");
+        Console.WriteLine("Character Report");
+        Console.WriteLine("===========");
+        Console.WriteLine("Name: " + playerName);
+        Console.WriteLine("Level: " + level);
+        Console.WriteLine("Health: " + health);
+        Console.WriteLine("Gold: " + gold);
+        Console.WriteLine("Speed: " + speed);
+        Console.WriteLine();
+        Console.WriteLine("After upgrade:");
+        Console.WriteLine("Level: " + levelAfter);
+        Console.WriteLine("Health: " + healthfter);
+        Console.WriteLine("Gold: " + goldAfrer);
+        
     }
 }
