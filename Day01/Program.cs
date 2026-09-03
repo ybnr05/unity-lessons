91ab3a8261bd3f3c819a3c5f4a4c50d1da6398da
ybnr05 <ybnr05@gmail.com>
2026-09-03 13:12:05 -0400

    day01.2

--- Day01/Program.cs
+++ Day01/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main()
     {
-        Console.WriteLine("Hello, Unity Developer!");
+        string playerName = "Bhuvan";
+        int level =5;
+        float speed = 4.5f;
+        bool hasSword = false;
+
+        Console.WriteLine(playerName);
+        Console.WriteLine(level);
+        Console.WriteLine(speed);
+        Console.WriteLine(hasSword);
     }
 }
