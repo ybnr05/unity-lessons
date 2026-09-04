5ae4d87ecf22c3a5c90d28ac4c5228ab356bfc58
ybnr05 <ybnr05@gmail.com>
2026-09-04 11:02:32 -0400

    switch01

--- Day01/Program.cs
+++ Day01/Program.cs
@@ -5,40 +5,24 @@ class Program
     
     static void Main()
     {
-        Console.Write("Enter player name: ");
-        string playername = Console.ReadLine()!;
-        Console.Write("Enter Health");
-        int health = int.Parse(Console.ReadLine()!);
-
-        if (health > 0)
-        {
-            Console.WriteLine("Status: Alive");
-        }
-        else
-        {
-            Console.WriteLine("Status: Dead");
-        }
-
-        Console.Write("Enter level");
-        int level = int.Parse(Console.ReadLine()!);
-        if (level>=10)
-        {
-            Console.WriteLine("Rank: Best player");
-        }
-        else
-        {
-            Console.WriteLine("Rank: worst player");
-        }
-        
-        Console.WriteLine();
-        Console.WriteLine("=========================");
-        Console.WriteLine("PLAYER REPORT");
-        Console.WriteLine("=========================");
-        
-
-        Console.WriteLine("Name:" + playername);
-        Console.WriteLine("Health:" + health);
-        Console.WriteLine("Level" + level);
+       Console.WriteLine("1.Warrior");
+       Console.WriteLine("2.Mage");
+       Console.WriteLine("3.Archer");
+       Console.Write("choose your class:");
+       int choice = int.Parse(Console.ReadLine());
+       
+       switch(choice)
+       {
+        case 1:
+            Console.WriteLine("You choose Warrior");
+            break;
+            case 2:
+            Console.WriteLine("you choose Mage");
+            break;
+            case 3:
+            Console.WriteLine("you choose Archer");
+            break;
+       }
         
         
     }
