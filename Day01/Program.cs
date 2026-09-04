4f7813702003d36f4804971d752a6760021aa957
ybnr05 <ybnr05@gmail.com>
2026-09-03 23:30:19 -0400

    player report

--- Day01/Program.cs
+++ Day01/Program.cs
@@ -4,35 +4,24 @@ class Program
 {
     static void Main()
     {
-        Console.Write("Enter your name:");
-        string playerName = Console.ReadLine()!;
-        Console.Write("Enter  level: ");
-        int level = int.Parse(Console.ReadLine()!);
-
-        Console.Write("Enter Health:");
-        int health = int.Parse(Console.ReadLine()!);
-        Console.Write("Enter Gold");
-        int gold = int.Parse(Console.ReadLine()!);
-        Console.Write("Enter Speed:");
-        int speed = int.Parse(Console.ReadLine()!);
-
-        int levelAfter = level + 1;
-        int healthfter = health + 10;
-        int goldAfrer = gold + 50;
+        Console.Write("Enter player name: ");
+        string playername = Console.ReadLine();
+        Console.Write("Enter Health");
+        int health = int.Parse(Console.ReadLine());
+        Console.Write("Enter level");
+        int level = int.Parse(Console.ReadLine());
+        
+        
         Console.WriteLine();
-        Console.WriteLine("===========");
-        Console.WriteLine("Character Report");
-        Console.WriteLine("===========");
-        Console.WriteLine("Name: " + playerName);
-        Console.WriteLine("Level: " + level);
-        Console.WriteLine("Health: " + health);
-        Console.WriteLine("Gold: " + gold);
-        Console.WriteLine("Speed: " + speed);
-        Console.WriteLine();
-        Console.WriteLine("After upgrade:");
-        Console.WriteLine("Level: " + levelAfter);
-        Console.WriteLine("Health: " + healthfter);
-        Console.WriteLine("Gold: " + goldAfrer);
+        Console.WriteLine("=========================");
+        Console.WriteLine("PLAYER REPORT");
+        Console.WriteLine("=========================");
+        
+
+        Console.WriteLine("Name:" + playername);
+        Console.WriteLine("Health:" + health);
+        Console.WriteLine("Level" + level);
+        
         
     }
 }
