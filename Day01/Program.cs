72e1c3f6c283fa7ea694a29150f542bf9640b2c4
ybnr05 <ybnr05@gmail.com>
2026-09-08 22:03:24 -0400

    switch calcultor

--- Day01/Program.cs
+++ Day01/Program.cs
@@ -5,23 +5,46 @@ class Program
     
     static void Main()
     {
-       Console.WriteLine("1.Warrior");
-       Console.WriteLine("2.Mage");
-       Console.WriteLine("3.Archer");
-       Console.Write("choose your class:");
+       Console.WriteLine("====Simple calculator====");
+       Console.WriteLine("1. Addition");
+       Console.WriteLine("2. Subtraction");
+       Console.WriteLine("3. Multiplication");
+       Console.WriteLine("4. Division");
+
+       Console.Write("Choose an option (1-4):");
        int choice = int.Parse(Console.ReadLine());
-       
-       switch(choice)
-       {
-        case 1:
-            Console.WriteLine("You choose Warrior");
-            break;
+        Console.Write("Enter first number:");
+        double num1 = double.Parse(Console.ReadLine());
+        Console.Write("Enter second number:");
+        double num2 = double.Parse(Console.ReadLine());
+        switch (choice)
+        {
+            case 1:
+                Console.WriteLine("Result = " + (num1 + num2));
+                break;
+
             case 2:
-            Console.WriteLine("you choose Mage");
-            break;
+                Console.WriteLine("Result = " + (num1 - num2));
+                break;
+
             case 3:
-            Console.WriteLine("you choose Archer");
-            break;
+                Console.WriteLine("Result = " + (num1 * num2));
+                break;
+
+            case 4:
+                if (num2 != 0)
+                {
+                    Console.WriteLine("Result = " + (num1 / num2));
+                }
+                else
+                {
+                    Console.WriteLine("Error: Cannot divide by zero.");
+                }
+                break;
+
+            default:
+                Console.WriteLine("Invalid choice.");
+                break;
        }
         
         
