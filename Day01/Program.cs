8a5799fdc326ef2d8f6580881235be6ad129a0c0
ybnr05 <ybnr05@gmail.com>
2026-09-09 15:12:08 -0400

    increasing and decreasing stars loop02

--- Day01/Program.cs
+++ Day01/Program.cs
@@ -4,12 +4,16 @@ class Program
 {
     static void Main()
     {
-        Console.Write("Enter a number: ");
-        int num = int.Parse(Console.ReadLine()!);
-
-        for (int i = 1; i <= 10; i++)
+        for (int i = 10; i >= 1; i--)
         {
-            Console.WriteLine($"{num} X {i} = {num * i}");
+            
+
+            for (int j = 1; j <= i; j++)
+            {
+                Console.Write("*");
+
+            }
+            Console.WriteLine();
         }
     }
 }
