425f34b8ad2163803004f0492efc479606761c51
ybnr05 <ybnr05@gmail.com>
2026-09-10 12:30:56 -0400

    while loop

--- Day01/Program.cs
+++ Day01/Program.cs
@@ -4,16 +4,11 @@ class Program
 {
     static void Main()
     {
-        for (int i = 10; i >= 1; i--)
+        int i = 1;
+        while (i<=10)
         {
-            
-
-            for (int j = 1; j <= i; j++)
-            {
-                Console.Write("*");
-
-            }
-            Console.WriteLine();
+            Console.WriteLine(i);
+            i++;
         }
     }
 }
