a3cecd22ff9c5f49f9f44e50ef78ce060fc1bcd1
ybnr05 <ybnr05@gmail.com>
2026-09-13 20:15:28 -0400

    calcu;lator using methods

--- Day01/Program.cs
+++ Day01/Program.cs
@@ -2,53 +2,74 @@
 
 class Program
 {
-    static void PrintHeader()
+    static double Add(double a, double b)
     {
-        Console.WriteLine("=========================");
-        Console.WriteLine("STUDENT REPORT");
-        Console.WriteLine("=========================");
+        return a + b;
     }
 
-    static void DisplayStudent(string name, int marks)
+    static double Subtract(double a, double b)
     {
-        Console.WriteLine("Name  : " + name);
-        Console.WriteLine("Marks : " + marks);
+        return a - b;
     }
 
-    static string CalculateGrade(int marks)
+    static double Multiply(double a, double b)
     {
-        if (marks >= 90)
+        return a * b;
+    }
+
+    static double Divide(double a, double b)
+    {
+        if (b == 0)
         {
-            return "A";
+            Console.WriteLine("Cannot divide by zero.");
+            return 0;
         }
-        else if (marks >= 75)
-        {
-            return "B";
-        }
-        else if (marks >= 60)
-        {
-            return "C";
-        }
-        else
-        {
-            return "F";
-        }
+
+        return a / b;
     }
 
     static void Main()
     {
-        Console.Write("Enter Student Name: ");
-        string name = Console.ReadLine();
+        Console.WriteLine("===== CALCULATOR =====");
+        Console.WriteLine("1. Add");
+        Console.WriteLine("2. Subtract");
+        Console.WriteLine("3. Multiply");
+        Console.WriteLine("4. Divide");
 
-        Console.Write("Enter Marks: ");
-        int marks = int.Parse(Console.ReadLine());
+        Console.Write("Choose an option: ");
+        int choice = int.Parse(Console.ReadLine());
 
-        PrintHeader();
+        Console.Write("Enter first number: ");
+        double num1 = double.Parse(Console.ReadLine());
 
-        DisplayStudent(name, marks);
+        Console.Write("Enter second number: ");
+        double num2 = double.Parse(Console.ReadLine());
 
-        string grade = CalculateGrade(marks);
+        double result = 0;
 
-        Console.WriteLine("Grade : " + grade);
+        switch (choice)
+        {
+            case 1:
+                result = Add(num1, num2);
+                break;
+
+            case 2:
+                result = Subtract(num1, num2);
+                break;
+
+            case 3:
+                result = Multiply(num1, num2);
+                break;
+
+            case 4:
+                result = Divide(num1, num2);
+                break;
+
+            default:
+                Console.WriteLine("Invalid Choice");
+                return;
+        }
+
+        Console.WriteLine("Result = " + result);
     }
 }
