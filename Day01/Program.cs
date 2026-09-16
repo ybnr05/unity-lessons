67d8cb61a593e101b1fc82aeb9e0f3a32a8d7216
ybnr05 <ybnr05@gmail.com>
2026-09-15 20:53:31 -0400

    how multiple class works class04

--- Day01/Program.cs
+++ Day01/Program.cs
@@ -1,26 +1,49 @@
 ﻿using System;
 
-class Student
+class Book
 {
-    public string Name;
-    public int Age;
-
-    public void Introduce()
-    {
-        Console.WriteLine("Hello, my name is " + Name);
-        Console.WriteLine("I am " + Age + " years old.");
-    }
+    public string Title;
+    public string Author;
+    public double Price;
 }
 
 class Program
 {
     static void Main()
     {
-        Student student1 = new Student();
+        Book book1 = new Book();
+        book1.Title = "Clean Code";
+        book1.Author = "Robert Martin";
+        book1.Price = 45.99;
 
-        student1.Name = "Bhuvan";
-        student1.Age = 22;
+        Book book2 = new Book();
+        book2.Title = "The Pragmatic Programmer";
+        book2.Author = "Andrew Hunt";
+        book2.Price = 49.99;
 
-        student1.Introduce();
+        Book book3 = new Book();
+        book3.Title = "Head First C#";
+        book3.Author = "Andrew Stellman";
+        book3.Price = 39.99;
+
+        Console.WriteLine("=====================");
+        Console.WriteLine("BOOK DETAILS");
+        Console.WriteLine("=====================");
+
+        Console.WriteLine("Title : " + book1.Title);
+        Console.WriteLine("Author: " + book1.Author);
+        Console.WriteLine("Price : $" + book1.Price);
+
+        Console.WriteLine();
+
+        Console.WriteLine("Title : " + book2.Title);
+        Console.WriteLine("Author: " + book2.Author);
+        Console.WriteLine("Price : $" + book2.Price);
+
+        Console.WriteLine();
+
+        Console.WriteLine("Title : " + book3.Title);
+        Console.WriteLine("Author: " + book3.Author);
+        Console.WriteLine("Price : $" + book3.Price);
     }
 }
