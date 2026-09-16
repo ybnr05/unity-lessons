809d6ef2e3841c1b1bc121825b22b6abdfa0af86
ybnr05 <ybnr05@gmail.com>
2026-09-15 20:42:47 -0400

    class02 multiple outcomes

--- Day01/Program.cs
+++ Day01/Program.cs
@@ -15,7 +15,19 @@ class Program
         student1.Name = "Bhuvan";
         student1.Age = 22;
 
+        Student student2 = new Student();
+
+        student2.Name = "Alex";
+        student2.Age = 20;
+
+        Console.WriteLine("Student 1");
         Console.WriteLine("Name: " + student1.Name);
         Console.WriteLine("Age : " + student1.Age);
+
+        Console.WriteLine();
+
+        Console.WriteLine("Student 2");
+        Console.WriteLine("Name: " + student2.Name);
+        Console.WriteLine("Age : " + student2.Age);
     }
 }
