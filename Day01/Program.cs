b21d96cfc1e452c2be01424915ab0c5f1c1539ce
ybnr05 <ybnr05@gmail.com>
2026-09-15 21:04:18 -0400

    book pricing

--- Day01/Program.cs
+++ Day01/Program.cs
@@ -5,45 +5,37 @@ class Book
     public string Title;
     public string Author;
     public double Price;
+
+    public Book(string title, string author, double price)
+    {
+        Title = title;
+        Author = author;
+        Price = price;
+    }
+
+    public void DisplayBook()
+    {
+        Console.WriteLine("Title : " + Title);
+        Console.WriteLine("Author: " + Author);
+        Console.WriteLine("Price : $" + Price);
+        Console.WriteLine();
+    }
 }
 
 class Program
 {
     static void Main()
     {
-        Book book1 = new Book();
-        book1.Title = "Clean Code";
-        book1.Author = "Robert Martin";
-        book1.Price = 45.99;
-
-        Book book2 = new Book();
-        book2.Title = "The Pragmatic Programmer";
-        book2.Author = "Andrew Hunt";
-        book2.Price = 49.99;
-
-        Book book3 = new Book();
-        book3.Title = "Head First C#";
-        book3.Author = "Andrew Stellman";
-        book3.Price = 39.99;
+        Book book1 = new Book("Harry Potter", "J.K. Rowling", 45.99);
+        Book book2 = new Book("Rich Dad Poor Dad", "Robert Kiyosaki", 49.99);
+        Book book3 = new Book("The subtle Art of Not Giving a Fc*k", "Mark Manson", 39.99);
 
         Console.WriteLine("=====================");
         Console.WriteLine("BOOK DETAILS");
         Console.WriteLine("=====================");
 
-        Console.WriteLine("Title : " + book1.Title);
-        Console.WriteLine("Author: " + book1.Author);
-        Console.WriteLine("Price : $" + book1.Price);
-
-        Console.WriteLine();
-
-        Console.WriteLine("Title : " + book2.Title);
-        Console.WriteLine("Author: " + book2.Author);
-        Console.WriteLine("Price : $" + book2.Price);
-
-        Console.WriteLine();
-
-        Console.WriteLine("Title : " + book3.Title);
-        Console.WriteLine("Author: " + book3.Author);
-        Console.WriteLine("Price : $" + book3.Price);
+        book1.DisplayBook();
+        book2.DisplayBook();
+        book3.DisplayBook();
     }
 }
