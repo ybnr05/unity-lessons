f0e880e2ccf0e25090b2146d947aea9b71cb7b7b
ybnr05 <ybnr05@gmail.com>
2026-09-15 20:44:02 -0400

    class03 how class insde method

--- Day01/Program.cs
+++ Day01/Program.cs
@@ -4,6 +4,12 @@ class Student
 {
     public string Name;
     public int Age;
+
+    public void Introduce()
+    {
+        Console.WriteLine("Hello, my name is " + Name);
+        Console.WriteLine("I am " + Age + " years old.");
+    }
 }
 
 class Program
@@ -15,19 +21,6 @@ class Program
         student1.Name = "Bhuvan";
         student1.Age = 22;
 
-        Student student2 = new Student();
-
-        student2.Name = "Alex";
-        student2.Age = 20;
-
-        Console.WriteLine("Student 1");
-        Console.WriteLine("Name: " + student1.Name);
-        Console.WriteLine("Age : " + student1.Age);
-
-        Console.WriteLine();
-
-        Console.WriteLine("Student 2");
-        Console.WriteLine("Name: " + student2.Name);
-        Console.WriteLine("Age : " + student2.Age);
+        student1.Introduce();
     }
 }
