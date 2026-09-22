1d83cd281e964f7fc96700eb756acbd2df39b234
ybnr05 <ybnr05@gmail.com>
2026-09-22 19:43:21 -0400

    rock paper scissors

--- Day01/Program.cs
+++ Day01/Program.cs
@@ -1,41 +1,122 @@
-﻿namespace CodeMonkey.CSharpCourse.L1520_FizzBuzz;
+﻿namespace CodeMonkey.CSharpCourse.L1540_RockPaperScissors;
 
-public class FizzBuzz
+public class RockPaperScissors
 {
-    /* ** Fizz Buzz **
+    /* ** Rock Paper Scissors **
      *
-     * Loop through Numbers 1-100
-     * If Divisible by 3, print Fizz
-     * If Divisible by 5, print Buzz
-     * If Divisible by 3 AND 5, print FizzBuzz
-     * Every other case, print Number
+     * There are 3 Actions
+     * Rock, Paper, Scissors
      *
-     * Hint: Use the % modulo operator to get the remainder of the division
-     * (6 % 3) == 0
+     * Rock beats Scissors
+     * Scissors beats Paper
+     * Paper beats Rock
      *
-     * Write to the console using Console.WriteLine();
-     */
-    public FizzBuzz()
+     * Computer generates Action
+     * Asks player for input
+     * Prints result
+     *
+     * Hint: You can generate random numbers with new System.Random().Next(3);
+     *       Generates between 0 (inclusive) and 3 (exclusive)
+     * */
+    public RockPaperScissors()
     {
-        for (int i = 1; i <= 100; i++)
+        string computerAction;
+
+        switch (GenerateRandomNumber())
         {
-            if (i % 3 == 0 && i % 5 == 0)
-            {
-                Console.WriteLine("FizzBuzz");
-            }
-            else if (i % 3 == 0)
-            {
-                Console.WriteLine("Fizz");
-            }
-            else if (i % 5 == 0)
-            {
-                Console.WriteLine("Buzz");
-            }
-            else
-            {
-                Console.WriteLine(i);
-            }
+            default:
+            case 0:
+                computerAction = "Rock";
+                break;
+            case 1:
+                computerAction = "Paper";
+                break;
+            case 2:
+                computerAction = "Scissors";
+                break;
         }
+
+        Console.WriteLine("Computer has generated Action...");
+        Console.WriteLine("Choose your Action: (Rock, Paper, Scissors)");
+
+        string? playerInput = Console.ReadLine();
+        string playerAction = playerInput?.Trim() ?? string.Empty;
+
+        if (playerAction.Equals("rock", StringComparison.OrdinalIgnoreCase))
+        {
+            playerAction = "Rock";
+        }
+        else if (playerAction.Equals("paper", StringComparison.OrdinalIgnoreCase))
+        {
+            playerAction = "Paper";
+        }
+        else if (playerAction.Equals("scissors", StringComparison.OrdinalIgnoreCase))
+        {
+            playerAction = "Scissors";
+        }
+        else
+        {
+            Console.WriteLine("Invalid action. Please choose Rock, Paper, or Scissors.");
+            return;
+        }
+
+        Console.WriteLine("-");
+        Console.WriteLine("Player Plays <color=#ffff00>" + playerAction + "</color>");
+        Console.WriteLine("Computer Plays <color=#ffff00>" + computerAction + "</color>");
+
+        switch (playerAction)
+        {
+            default:
+                break;
+            case "Rock":
+                switch (computerAction)
+                {
+                    case "Rock":
+                        Console.WriteLine("<color=#00ffff>Tie</color>");
+                        break;
+                    case "Paper":
+                        Console.WriteLine("Paper beats Rock, <color=#ffff00>Computer wins!</color>");
+                        break;
+                    case "Scissors":
+                        Console.WriteLine("Rock beats Scissors, <color=#00ff00>Player wins!</color>");
+                        break;
+                }
+                break;
+            case "Paper":
+                switch (computerAction)
+                {
+                    case "Rock":
+                        Console.WriteLine("Paper beats Rock, <color=#00ff00>Player wins!</color>");
+                        break;
+                    case "Paper":
+                        Console.WriteLine("<color=#00ffff>Tie</color>");
+                        break;
+                    case "Scissors":
+                        Console.WriteLine("Scissors beats Paper, <color=#ffff00>Computer wins!</color>");
+                        break;
+                }
+                break;
+            case "Scissors":
+                switch (computerAction)
+                {
+                    case "Rock":
+                        Console.WriteLine("Rock beats Scissors, <color=#ffff00>Computer wins!</color>");
+                        break;
+                    case "Paper":
+                        Console.WriteLine("Scissors beats Paper, <color=#00ff00>Player wins!</color>");
+                        break;
+                    case "Scissors":
+                        Console.WriteLine("<color=#00ffff>Tie</color>");
+                        break;
+                }
+                break;
+        }
+    }
+
+    private int GenerateRandomNumber()
+    {
+        // Generates a random number between 0 (include) and 3 (exclusive)
+        return new Random().Next(3);
     }
 }
 
@@ -43,6 +124,6 @@ public class Program
 {
     public static void Main()
     {
-        _ = new FizzBuzz();
+        _ = new RockPaperScissors();
     }
 }
